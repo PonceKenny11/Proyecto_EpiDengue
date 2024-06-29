3f3b8c7dcf6bd38c3c30c90051ba3a2a339cf3b2
valerycanches <74372171+valerycanches@users.noreply.github.com>
2024-06-29 15:01:46 -0500

    añadiendo pasos para inicio de sesion

--- app/src/main/java/com/example/app_epidengue/FichaDengue.java
+++ app/src/main/java/com/example/app_epidengue/FichaDengue.java
@@ -15,7 +15,7 @@ public class FichaDengue extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_ficha_dengue);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.btningresar), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
--- app/src/main/java/com/example/app_epidengue/UbicacionInfeccion.java
+++ app/src/main/java/com/example/app_epidengue/UbicacionInfeccion.java
@@ -15,7 +15,7 @@ public class UbicacionInfeccion extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_ubicacion_infeccion);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.btningresar), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
