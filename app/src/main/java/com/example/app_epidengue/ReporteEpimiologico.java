1ac056ed635a8c7deee1383474be66745abc3151
Gustavo Ponce <132284919+PonceKenny11@users.noreply.github.com>
2024-07-16 21:48:03 -0500

    diseño mostrar ficcha

--- app/src/main/java/com/example/app_epidengue/ReporteEpimiologico.java
+++ app/src/main/java/com/example/app_epidengue/ReporteEpimiologico.java
@@ -1,12 +1,6 @@
 package com.example.app_epidengue;
 
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TableLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,18 +8,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.app_epidengue.Repository.DiagnosticoDB;
-import com.example.app_epidengue.Repository.RegistrarPacientBD;
-
 public class ReporteEpimiologico extends AppCompatActivity {
 
-    private EditText etSearchDNI;
 
-    private TableLayout tableLayout;
-    private TextView tvNombreCompleto, tvEdad, tvSexo;
-
-    private RegistrarPacientBD pacientBD;
-    private DiagnosticoDB diagnosticoDB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,64 +22,9 @@ public class ReporteEpimiologico extends AppCompatActivity {
             return insets;
         });
 
-        etSearchDNI = findViewById(R.id.txtSearchDNI);
-        tableLayout = findViewById(R.id.tableLayout);
-        tvNombreCompleto = findViewById(R.id.tvNombreCompleto);
-        tvEdad = findViewById(R.id.tvEdad);
-        tvSexo = findViewById(R.id.tvSexo);
 
-        pacientBD = new RegistrarPacientBD(this);
-        diagnosticoDB = new DiagnosticoDB(this);
 
 
     }
 
-
-    public void BuscarPaciente(View view){
-        String buscardni = etSearchDNI.getText().toString().trim();
-        if (TextUtils.isEmpty(buscardni)) {
-            Toast.makeText(this, "Por favor, ingrese un DNI", Toast.LENGTH_SHORT).show();
-            etSearchDNI.setText("");
-            etSearchDNI.requestFocus();
-        }
-
-        /*boolean encontro = pacientBD.searchPatient(buscardni, tvNombreCompleto,tvEdad, tvSexo);
-        if(encontro){
-            //tableLayout.setVisibility(View.VISIBLE);
-            Toast.makeText(this, "Paciente  encontrado", Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(this, "Paciente no encontrado", Toast.LENGTH_SHORT).show();
-            tableLayout.setVisibility(View.GONE);
-        }*/
-    }
-
-    public void EliminarPaciente(View view){
-        String dni = etSearchDNI.getText().toString().trim();
-        try {
-            boolean isDeleted = pacientBD.deletePatientByDNI(dni);
-            if (isDeleted) {
-                Toast.makeText(this, "Paciente eliminado exitosamente", Toast.LENGTH_SHORT).show();
-                tableLayout.setVisibility(View.GONE);
-            } else {
-                Toast.makeText(this, "Error al eliminar el paciente", Toast.LENGTH_SHORT).show();
-            }
-        } catch (Exception e) {
-            Toast.makeText(this, "Catch al eliminar el paciente: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    public void EliminarDiagnostico(View view){
-        String dni = etSearchDNI.getText().toString().trim();
-        try {
-            boolean isDeleted = diagnosticoDB.deleteDiagnosticoByName(dni);
-            if (isDeleted) {
-                Toast.makeText(this, "Dianosticos eliminado exitosamente", Toast.LENGTH_SHORT).show();
-                tableLayout.setVisibility(View.GONE);
-            } else {
-                Toast.makeText(this, "Error al eliminar el diagnostico", Toast.LENGTH_SHORT).show();
-            }
-        } catch (Exception e) {
-            Toast.makeText(this, "Catch al eliminar el diagnostico: " + e.getMessage(), Toast.LENGTH_SHORT).show();
-        }
-    }
 }
